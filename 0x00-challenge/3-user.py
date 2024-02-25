c9f0d620f7aa63c145339a6bfac0aed3b83c0740
zfiromsa <mdzm841@gmail.com>
2024-02-25 11:58:03 -0800

    go

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -12,14 +12,13 @@ class User():
     - id: public string unique (uuid)
     - password: private string hash in MD5
     """
-
+    __password = None
     def __init__(self):
         """
         Initialize a new user:
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
-        self.__password = None
 
     @property
     def password(self):
