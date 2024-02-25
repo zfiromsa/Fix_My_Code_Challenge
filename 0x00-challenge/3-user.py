527e6a62d8459bf6fb39df604e7596eb0de9c166
zfiromsa <mdzm841@gmail.com>
2024-02-25 12:01:43 -0800

    go

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -48,11 +48,11 @@ class User():
         - `False` if `__password` is `None`
         - Compare `__password` and the MD5 value of `pwd`
         """
-        if pwd is None or isinstance(pwd, str):
+        if pwd is None or not isinstance(pwd, str):
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
